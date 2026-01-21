5957753fc3b0f133f7fdcba65393a9f185c436bf
franekruzicevic <frane.kruzicevic@gmail.com>
2026-01-21 20:34:08 +0100

    Add project files.

---
+++ Dorian_Khan_Frane_Kruzicevic_RezervacijeSobe/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Dorian_Khan_Frane_Kruzicevic_RezervacijeSobe
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
