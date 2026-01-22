1e0850a71c2fc6b517fffbdb18b4eebe6ac6b7f6
anatomicallyaccurate <ccrwgn@pm.me>
2026-01-22 01:56:42 +0100

    fd

---
+++ Dorian_Khan_Frane_Kruzicevic_RezervacijeSobe/Soba245.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dorian_Khan_Frane_Kruzicevic_RezervacijeSobe
+{
+    /// <summary>
+    /// Interaction logic for Soba245.xaml
+    /// </summary>
+    public partial class Soba245 : Window
+    {
+        public Soba245()
+        {
+            InitializeComponent();
+        }
+    }
+}
